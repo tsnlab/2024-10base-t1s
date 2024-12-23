a597ff7880878f4133576cefbeb5fa20fb62e94e
jsohye <junseok.ohye@gmail.com>
2024-12-23 16:52:11 +0900

    Fix build errors

---
+++ arp-test/inc/spi.h
@@ -0,0 +1,18 @@
+#ifndef SPI_H
+#define SPI_H
+
+// sudo apt install pigpio
+#include <pigpio.h>
+
+typedef enum {
+	SPI_E_SUCCESS = 0,
+	SPI_E_UNKNOWN_ERROR,
+	SPI_E_INIT_ERROR,
+	SPI_E_ISR_INIT_ERROR,
+} SPI_ReturnType;
+
+SPI_ReturnType SPI_Init(void);
+SPI_ReturnType SPI_Transfer(uint8_t* rxBuffer, uint8_t* txBuffer, uint16_t length);
+SPI_ReturnType SPI_Cleanup(void);
+
+#endif /* SPI_H */
--- arp-test/spi.c
+++ arp-test/spi.c
@@ -1,8 +1,6 @@
 #include <arp_test.h>
 #include <hardware_dependent.h>
-
-// sudo apt install pigpio
-#include <pigpio.h>
+#include <spi.h>
 
 #define DIO0_GPIO_PIN                (uint32_t)23       //!< GPIO pin 23
 #define MACPHY_INTERRUPT_GPIO_PIN    (uint32_t)25       //!< GPIO pin 25
@@ -15,11 +13,13 @@
 static int spiHandle;
 
 //Configures to send and recieve SPI data to the NCN26010. Return true on success
-uint8_t SPI_Init() {
+SPI_ReturnType SPI_Init(void) {
+
   gpioCfgClock(1, 1, 1); //Settup sample rate to be 1 MHz.
   if (gpioInitialise() < 0) {
       return SPI_E_ISR_INIT_ERROR;
   }
+
   spiHandle = spiOpen(SPI_CHANNEL, SPI_COMM_SPEED, SPI_FLAGS);
   if (spiHandle > 0) {
       return SPI_E_SUCCESS;
@@ -30,10 +30,10 @@ uint8_t SPI_Init() {
 }
 
 //transfer data over spi. Return true on success
-uint8_t SPI_Transfer(uint8_t* rxBuffer, uint8_t* txBuffer, uint16_t numBytesToTransfer) {
+SPI_ReturnType SPI_Transfer(uint8_t* rxBuffer, uint8_t* txBuffer, uint16_t length) {
     int numTransfered = -1;
 
-    numTransfered = spiXfer(spiHandle, (char*)txBuffer, (char*)rxBuffer, numBytesToTransfer);
+    numTransfered = spiXfer(spiHandle, (char*)txBuffer, (char*)rxBuffer, length);
     if (numTransfered > 0) {
         return SPI_E_SUCCESS;
     }
@@ -42,8 +42,8 @@ uint8_t SPI_Transfer(uint8_t* rxBuffer, uint8_t* txBuffer, uint16_t numBytesToTr
     }
 }
 
-uint8_t SPI_Cleanup() { 
-    uint8_t ret = SPI_E_UNKNOWN_ERROR;
+SPI_ReturnType SPI_Cleanup(void) { 
+    SPI_ReturnType ret = SPI_E_UNKNOWN_ERROR;
     
     if (spiHandle > 0) {
         spiClose(spiHandle);
