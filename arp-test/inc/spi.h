9373ba418d71c85376133abf92fce708b1d2ffe9
jsohye <junseok.ohye@gmail.com>
2024-12-30 18:11:41 +0900

    add register read/write feature (with debug code for now)

--- arp-test/inc/spi.h
+++ arp-test/inc/spi.h
@@ -19,6 +19,9 @@ SPI_ReturnType SPI_Init(void);
 SPI_ReturnType SPI_Transfer(uint8_t* rxBuffer, uint8_t* txBuffer, uint16_t length);
 SPI_ReturnType SPI_Cleanup(void);
 
+uint32_t ReadRegister(uint8_t MMS, uint16_t Address);
+uint32_t WriteRegister(uint8_t MMS, uint16_t Address, uint32_t data);
+
 bool GetParity(uint32_t valueToCalculateParity);
 void ConvertEndianness(uint32_t valueToConvert, uint32_t* convertedValue);
 
--- arp-test/main.c
+++ arp-test/main.c
@@ -5,6 +5,10 @@
 int main(int argc, char* argv[]) {
     ARP_ReturnType arpRet = ARP_E_UNKNOWN_ERROR;
     SPI_ReturnType spiRet = SPI_E_UNKNOWN_ERROR;
+    uint8_t MMS = 0u;
+    uint16_t Address = 0x0004u;
+    uint32_t data = 0u;
+    bool executionStatus = false;
     // PLCA setting must be done before arp_test
 
     spiRet = SPI_Init();
@@ -12,7 +16,28 @@ int main(int argc, char* argv[]) {
         printf("SPI_Init failed; the error code is %d\n", spiRet);
     }
 
-    /*     arpRet = ArpTest();
+    data = ReadRegister(MMS, Address);
+    printf("read data from MMS %d, Address %d is 0x%08X\n", MMS, Address, data);
+
+    MMS = 1u;
+    Address = 0x0000u;
+
+    data = ReadRegister(MMS, Address);
+    printf("read data before writing to MMS %d, Address %d is 0x%08X\n", MMS, Address, data);
+
+    data = 0x103u;
+    executionStatus = WriteRegister(MMS, Address, data);
+    if (executionStatus == true) {
+        printf("Write Register to MMS %d, Address %d is successful\n", MMS, Address);
+    } else {
+        printf("Write Register to MMS %d, Address %d is failed\n", MMS, Address);
+    }
+
+    data = ReadRegister(MMS, Address);
+    printf("read data after writing to MMS %d, Address %d is 0x%08X\n", MMS, Address, data);
+
+    /*
+        arpRet = ArpTest();
         printf("Result of arp_test is %d\n", arpRet);
      */
     spiRet = SPI_Cleanup();
